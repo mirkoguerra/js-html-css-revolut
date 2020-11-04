b1d6eb890a3c75017ec4b1a087f6e1194548cfc7
Mirko Guerra <mirkoguerra93@gmail.com>
2020-11-04 16:40:40 +0100

    Incompleto, mal impostato

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
   $("section.nav-bar div.right-nav-bar div.links a:first-of-type").click(function(){
-    $("section.ghost-boxes div.features-box").toggleClass("visible");
+    $("section.ghost-boxes div.features-box").toggle();
   });
 
 });
