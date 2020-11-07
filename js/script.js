869d077ac3bbad529d2320c42f914943f84b87ba
Mirko Guerra <mirkoguerra93@gmail.com>
2020-11-07 15:19:48 +0100

    Miglioramento parte js

--- js/script.js
+++ js/script.js
@@ -2,10 +2,21 @@ $(document).ready(function(){
 
   $('.has-dropdown').click(function(){
 
-        $('.has-dropdown').children().removeClass('active');
+        // al click su uno dei link, rendo invisibili i figli dei fratelli dell'elenco di link
+        $(this).siblings().children('.dropdown').removeClass("active");
 
-        $(this).children().addClass('active');
+        // al click su uno dei link, faccio apparire / scomparire suo figlio
+        $(this).children('.dropdown').toggleClass('active');
 
-    })
+    });
+
+  $(document).click(function(event){
+
+    // se viene cliccato un punto della pagina che non ha il tag <a>, la classe active viene rimossa dagli elementi che hanno la classe dropdown
+    if (event.target.tagName !== 'A') {
+      $('.dropdown').removeClass('active');
+    }
+
+  });
 
 });
